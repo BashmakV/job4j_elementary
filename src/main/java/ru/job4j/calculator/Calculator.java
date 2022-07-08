f7483ed4d3fe17b3ba5cf12d1767a8e931e3298e
BashmakV <dolka-lenka@mail.ru>
2022-07-08 12:29:16 +0300

    3.2. Загрузка файлов на github [#243562}]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -5,20 +5,20 @@ public class Calculator {
         int one = 1;
         int two = 2;
         int onePlusTwo = one + two;
-        *//*System.out.println(onePlusTwo);*//*
+        System.out.println(onePlusTwo);
         int four = 4;
         int five = 5;
         int six = 6;
         int fourTimeTwo = four * two;
         int fiveMinusTwo = five - two;
         int sixDivTwo = six / two;
-       *//* System.out.println(sixDivTwo);
+        System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);*//*
+        System.out.println(fourTimeTwo);
         int size = 10;
         size = 100;
         size = size - 5;
-        *//*System.out.println(size);  *//*
+        System.out.println(size);
     }*/
 
     public static void plus(int first, int second) {
