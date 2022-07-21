38c1863c7c12ba7650734e61763be6120dcf0a0e
BashmakV <dolka-lenka@mail.ru>
2022-07-21 14:50:13 +0300

    3.2. Загрузка файлов на github [#243562}]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -36,16 +36,12 @@ public class Calculator {
         return y;
     }
 
-    public static int func2(int x) {
-        int y = 1 / x;
-        return y;
-    }
+
 
 
     public static void main(String[] args) {
         int result1 = Calculator.func1(100);
-        int result2 = Calculator.func2(5);
-        int result3 = result1 + result2;
+        int result3 = result1;
         System.out.println(result3);
     }
 }
