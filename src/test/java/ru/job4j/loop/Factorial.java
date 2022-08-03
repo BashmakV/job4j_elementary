d5eb4adf11cdf22ffe8d0fe2f5fc1984142222fe
BashmakV <dolka-lenka@mail.ru>
2022-08-03 12:24:06 +0300

    3.2. Загрузка файлов на github [#243562}]

--- src/test/java/ru/job4j/loop/Factorial.java
+++ src/test/java/ru/job4j/loop/Factorial.java
@@ -3,11 +3,8 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calc(int n) {
         int multiply = 1;
-        if (n == 0) {
-            return 1;
-        }
-        for (int index = 1; index <= n; index++) {
-          multiply = multiply * index;
+        for (int index = 2; index <= n; index++) {
+            multiply *= index;
         }
         return multiply;
     }
