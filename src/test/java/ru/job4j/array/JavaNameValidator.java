b82256872bff560a9a94c108db8d686bf3e83f07
BashmakV <dolka-lenka@mail.ru>
2022-08-09 19:39:59 +0300

    3.2. Загрузка файлов на github [#243562}]

---
+++ src/test/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean valid = false;
+        if (!name.isEmpty()) {
+            if (Character.isDigit(name.codePointAt(0)) || isUpperLatinLetter(name.codePointAt(0))) {
+                return valid;
+            }
+            for (int i = 0; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (isUpperLatinLetter(code) || isSpecialSymbol(code) || isLowerLatinLetter(code)) {
+                    valid = true;
+                }
+            }
+        }
+        return valid;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return Character.isUpperCase(code);
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return Character.isLowerCase(code);
+    }
+}
