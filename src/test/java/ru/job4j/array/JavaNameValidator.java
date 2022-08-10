6431bc5cd960e523ad61c99e1673d85bc46c2b7c
BashmakV <dolka-lenka@mail.ru>
2022-08-10 20:29:07 +0300

    3.2. Загрузка файлов на github [#243562}]

--- src/test/java/ru/job4j/array/JavaNameValidator.java
+++ src/test/java/ru/job4j/array/JavaNameValidator.java
@@ -3,15 +3,19 @@ package ru.job4j.array;
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
         boolean valid = false;
-        if (!name.isEmpty()) {
-            if (Character.isDigit(name.codePointAt(0)) || isUpperLatinLetter(name.codePointAt(0))) {
-                return valid;
-            }
-            for (int i = 0; i < name.length(); i++) {
-                int code = name.codePointAt(i);
-                if (isUpperLatinLetter(code) || isSpecialSymbol(code) || isLowerLatinLetter(code)) {
-                    valid = true;
-                }
+        if (name.isEmpty()) {
+            return valid;
+        }
+        if (Character.isDigit(name.codePointAt(0))) {
+            return valid;
+        }
+        if (isUpperLatinLetter(name.codePointAt(0))) {
+            return valid;
+        }
+        for (int i = 1; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (isUpperLatinLetter(code) || isSpecialSymbol(code) || isLowerLatinLetter(code)) {
+                valid = true;
             }
         }
         return valid;
