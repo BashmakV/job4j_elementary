5d86be359c20c1a1da1521c8214c02d7abb95660
BashmakV <dolka-lenka@mail.ru>
2022-08-11 16:54:52 +0300

    3.2. Загрузка файлов на github [#243562}]

--- src/test/java/ru/job4j/array/JavaNameValidator.java
+++ src/test/java/ru/job4j/array/JavaNameValidator.java
@@ -3,24 +3,14 @@ package ru.job4j.array;
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
         boolean valid = true;
-        if (name.isEmpty()) {
-            return false;
-        }
-        if (Character.isDigit(name.codePointAt(0))) {
-            return false;
-        }
-        if (isUpperLatinLetter(name.codePointAt(0))) {
-            return false;
-        }
-        for (int i = 1; i < name.length(); i++) {
-            int code = name.codePointAt(i);
-            if (!isUpperLatinLetter(code)) {
-                if (!isSpecialSymbol(code)) {
-                    if (!isLowerLatinLetter(code)) {
-                        if (!Character.isDigit(code)) {
-                            valid = false;
-                        }
-                    }
+        if (name.isEmpty() || Character.isDigit(name.codePointAt(0)) || isUpperLatinLetter(name.codePointAt(0))) {
+            valid = false;
+        } else {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                valid = isUpperLatinLetter(code) || isSpecialSymbol(code) || isLowerLatinLetter(code) || Character.isDigit(code);
+                if (!valid) {
+                    break;
                 }
             }
         }
@@ -38,4 +28,5 @@ public class JavaNameValidator {
     public static boolean isLowerLatinLetter(int code) {
         return Character.isLowerCase(code);
     }
+
 }
